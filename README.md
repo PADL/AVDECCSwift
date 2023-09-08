763c8d59409aaa22f1a138ca6bb4553c22c4b941
Luke Howard <lukeh@padl.com>
2023-09-08 15:31:24 +1000

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+AVDECCSwift
+===========
+
+AVDECCSwift is intended to be a Swift wrapper around the L-Acoustics [AVDECC library](https://github.com/L-Acoustics/avdecc).
+
--- Sources/AVDECCSwift/AVDECC.swift
+++ Sources/AVDECCSwift/AVDECC.swift
@@ -1,3 +1,51 @@
-
 import CAVDECC
+
+extension UnsafePointer {
+    func propertyBasePointer<Property>(to property: KeyPath<Pointee, Property>)
+        -> UnsafePointer<Property>?
+    {
+        guard let offset = MemoryLayout<Pointee>.offset(of: property) else { return nil }
+        return (UnsafeRawPointer(self) + offset).assumingMemoryBound(to: Property.self)
+    }
+}
+
+public final class AVDECC {
+    public var shared = AVDECC()
+
+    public static var interfaceVersion: avdecc_interface_version_t {
+        LA_AVDECC_getInterfaceVersion()
+    }
+
+    public static var version: String {
+        String(cString: LA_AVDECC_getVersion())
+    }
+
+    init() {
+        LA_AVDECC_initialize()
+    }
+
+    deinit {
+        LA_AVDECC_uninitialize()
+    }
+}
+
+public extension avdecc_unique_identifier_t {
+    static var null: Self {
+        LA_AVDECC_getNullUniqueIdentifier()
+    }
+}
+
+public extension String {
+    init(avdeccFixedString: avdecc_fixed_string_t) {
+        self.init(withUnsafePointer(to: avdeccFixedString) { pointer in
+            let start = pointer.propertyBasePointer(to: \.0)!
+            return start.withMemoryRebound(
+                to: UInt8.self,
+                capacity: MemoryLayout.size(ofValue: pointer)
+            ) {
+                String(cString: $0)
+            }
+        })
+    }
+}
 
