fab50c88771cc40aa214efde67b484b7bfcbc8ca
Luke Howard <lukeh@padl.com>
2023-09-12 12:42:35 +1000

    (c)

--- Sources/CxxAVDECC/shims/CxxAVDECCBlockShimsInternal.hpp
+++ Sources/CxxAVDECC/shims/CxxAVDECCBlockShimsInternal.hpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2023, PADL Software Pty Ltd
+ *
+ * This file is part of AVDECCSwift.
+ *
+ * AVDECCSwift is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AVDECCSwift is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AVDECCSwift.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include "CxxAVDECC.h"
